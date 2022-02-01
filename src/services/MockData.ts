ef64d0a28fa8ccc090f1b7ab3cc76fb838f62601
stanley2058 <stanley2058@yahoo.com.tw>
2022-02-01 16:31:01 +0800

    fix: update mock data

--- src/services/MockData.ts
+++ src/services/MockData.ts
@@ -7,9 +7,9 @@ const MockGraphData = {
       name: "details.book",
     },
     {
-      id: "details\tbook\tv1\t/details/0",
+      id: "details\tbook\tv1\t/*",
       group: "details\tbook",
-      name: "(details.book v1) /details/0",
+      name: "(details.book v1) /*",
     },
     {
       id: "reviews\tbook",
@@ -17,19 +17,19 @@ const MockGraphData = {
       name: "reviews.book",
     },
     {
-      id: "reviews\tbook\tv1\t/reviews/0",
+      id: "reviews\tbook\tv1\t/*",
       group: "reviews\tbook",
-      name: "(reviews.book v1) /reviews/0",
+      name: "(reviews.book v1) /*",
     },
     {
-      id: "reviews\tbook\tv3\t/reviews/0",
+      id: "reviews\tbook\tv3\t/*",
       group: "reviews\tbook",
-      name: "(reviews.book v3) /reviews/0",
+      name: "(reviews.book v3) /*",
     },
     {
-      id: "reviews\tbook\tv2\t/reviews/0",
+      id: "reviews\tbook\tv2\t/*",
       group: "reviews\tbook",
-      name: "(reviews.book v2) /reviews/0",
+      name: "(reviews.book v2) /*",
     },
     {
       id: "productpage\tbook",
@@ -42,9 +42,9 @@ const MockGraphData = {
       name: "(productpage.book v1) /productpage",
     },
     {
-      id: "productpage\tbook\tv1\t/static/bootstrap/css/bootstrap.min.css",
+      id: "productpage\tbook\tv1\t/static*",
       group: "productpage\tbook",
-      name: "(productpage.book v1) /static/bootstrap/css/bootstrap.min.css",
+      name: "(productpage.book v1) /static*",
     },
     {
       id: "ratings\tbook",
@@ -52,35 +52,23 @@ const MockGraphData = {
       name: "ratings.book",
     },
     {
-      id: "ratings\tbook\tv1\t/ratings/0",
+      id: "ratings\tbook\tv1\t/*",
       group: "ratings\tbook",
-      name: "(ratings.book v1) /ratings/0",
+      name: "(ratings.book v1) /*",
     },
   ],
   links: [
+    { source: "details\tbook", target: "details\tbook\tv1\t/*" },
+    { source: "reviews\tbook", target: "reviews\tbook\tv1\t/*" },
+    { source: "reviews\tbook", target: "reviews\tbook\tv3\t/*" },
     {
-      source: "details\tbook",
-      target: "details\tbook\tv1\t/details/0",
+      source: "reviews\tbook\tv3\t/*",
+      target: "ratings\tbook\tv1\t/*",
     },
+    { source: "reviews\tbook", target: "reviews\tbook\tv2\t/*" },
     {
-      source: "reviews\tbook",
-      target: "reviews\tbook\tv1\t/reviews/0",
-    },
-    {
-      source: "reviews\tbook",
-      target: "reviews\tbook\tv3\t/reviews/0",
-    },
-    {
-      source: "reviews\tbook\tv3\t/reviews/0",
-      target: "ratings\tbook\tv1\t/ratings/0",
-    },
-    {
-      source: "reviews\tbook",
-      target: "reviews\tbook\tv2\t/reviews/0",
-    },
-    {
-      source: "reviews\tbook\tv2\t/reviews/0",
-      target: "ratings\tbook\tv1\t/ratings/0",
+      source: "reviews\tbook\tv2\t/*",
+      target: "ratings\tbook\tv1\t/*",
     },
     {
       source: "productpage\tbook",
@@ -88,33 +76,27 @@ const MockGraphData = {
     },
     {
       source: "productpage\tbook\tv1\t/productpage",
-      target: "reviews\tbook\tv2\t/reviews/0",
+      target: "reviews\tbook\tv2\t/*",
     },
     {
       source: "productpage\tbook\tv1\t/productpage",
-      target: "reviews\tbook\tv3\t/reviews/0",
+      target: "reviews\tbook\tv3\t/*",
     },
     {
       source: "productpage\tbook\tv1\t/productpage",
-      target: "reviews\tbook\tv1\t/reviews/0",
+      target: "reviews\tbook\tv1\t/*",
     },
     {
       source: "productpage\tbook\tv1\t/productpage",
-      target: "details\tbook\tv1\t/details/0",
+      target: "details\tbook\tv1\t/*",
     },
     { source: "null", target: "productpage\tbook\tv1\t/productpage" },
     {
       source: "productpage\tbook",
-      target: "productpage\tbook\tv1\t/static/bootstrap/css/bootstrap.min.css",
+      target: "productpage\tbook\tv1\t/static*",
     },
-    {
-      source: "null",
-      target: "productpage\tbook\tv1\t/static/bootstrap/css/bootstrap.min.css",
-    },
-    {
-      source: "ratings\tbook",
-      target: "ratings\tbook\tv1\t/ratings/0",
-    },
+    { source: "null", target: "productpage\tbook\tv1\t/static*" },
+    { source: "ratings\tbook", target: "ratings\tbook\tv1\t/*" },
   ],
   services: [
     "details\tbook",
