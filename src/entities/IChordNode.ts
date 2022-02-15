02cf0f64c922c06b23232993b9c2dc239d40010e
stanley2058 <stanley2058@yahoo.com.tw>
2022-02-15 21:29:27 +0800

    feat: add chord data define

---
+++ src/entities/IChordNode.ts
@@ -0,0 +1,5 @@
+export default interface IChordNode {
+  id: string;
+  name: string;
+  fill: string;
+}
---
+++ src/entities/IChordRadius.ts
@@ -0,0 +1,5 @@
+export default interface IChordRadius {
+  from: string;
+  to: string;
+  value: number;
+}
