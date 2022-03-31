aa09c12cd07038bad718acfcd87faf62d3e41d18
stanley2058 <stanley2058@yahoo.com.tw>
2022-03-31 14:38:05 +0800

    feat: add lazy loading by route

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import DependencyGraph from "./pages/DependencyGraph";
-import Metrics from "./pages/Metrics";
-import Swagger from "./pages/Swagger";
-import Insights from "./pages/Insights";
-import Labels from "./pages/Labels";
-import Interfaces from "./pages/Interfaces";
+import { CircularProgress } from "@mui/material";
+
+const DependencyGraph = lazy(() => import("./pages/DependencyGraph"));
+const Metrics = lazy(() => import("./pages/Metrics"));
+const Insights = lazy(() => import("./pages/Insights"));
+const Labels = lazy(() => import("./pages/Labels"));
+const Interfaces = lazy(() => import("./pages/Interfaces"));
+const Swagger = lazy(() => import("./pages/Swagger"));
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<DependencyGraph />} />
-        <Route path="/metrics" element={<Metrics />} />
-        <Route path="/insights" element={<Insights />} />
-        <Route path="/labels" element={<Labels />} />
-        <Route path="/interfaces" element={<Interfaces />} />
-        <Route path="/swagger/:service" element={<Swagger />} />
-      </Routes>
+      <Suspense fallback={<CircularProgress />}>
+        <Routes>
+          <Route path="/" element={<DependencyGraph />} />
+          <Route path="/metrics" element={<Metrics />} />
+          <Route path="/insights" element={<Insights />} />
+          <Route path="/labels" element={<Labels />} />
+          <Route path="/interfaces" element={<Interfaces />} />
+          <Route path="/swagger/:service" element={<Swagger />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
