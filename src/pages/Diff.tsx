69032fde5989798b81b9046ecac7ba058562134c
wys899195 <wys899195@gmail.com>
2025-04-14 07:59:48 +0800

    fix: tag creation time format on Diff page

--- src/pages/Diff.tsx
+++ src/pages/Diff.tsx
@@ -165,15 +165,7 @@ export default function Diff() {
     if (!timestamp) {
       return ""; 
     }
-    const formatedTimestamp = new Date(timestamp).toLocaleString("zh-TW", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false, // 24 hour format
-    });
+    const formatedTimestamp = new Date(timestamp).toLocaleString();
     return `(Created at ${formatedTimestamp})`
   };
   /***constants for diff version control***/
